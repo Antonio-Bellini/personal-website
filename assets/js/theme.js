cc26483a525e093373b3acb291e66faa784744f1
Ant00o <belliniantonio205@gmail.com>
2022-05-30 17:40:59 +0200

    Modifiche alla home

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -1,22 +1,3 @@
-import 'assets/style/style.css'
-
-const themeButton = document.querySelector('.theme');
-
-function getCurrentTheme() {
-   let theme = window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light';
-    return theme;
+function test() {
+    alert("ciao");
 }
-
-function loadTheme(theme) {
-    const root = document.querySelector(':root');
-    if (theme === "light") {
-        themeButton.innerHTML = '🌙';
-    } else {
-        themeButton.innerHTML = '☀️';
-    }
-    root.setAttribute('color-scheme', )
-}
-
-window.addEventListener('DOMContentLoaded', () => {
-    loadTheme(getCurrentTheme());
-})
