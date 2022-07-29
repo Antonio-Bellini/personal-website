5a4bc9df684880f061b15505c773351c9bb3d12d
Ant00o <belliniantonio205@gmail.com>
2022-07-29 23:02:33 +0200

    Correzzione

--- assets/js/showMore.js
+++ assets/js/showMore.js
@@ -1,7 +1,7 @@
 const showMoreBtn = document.querySelector(".showMoreBtn");
 const moreWorks = document.querySelector(".moreWorks");
 
-showMoreBtn.addEventListener('click',(e)=> {
+showMoreBtn.addEventListener('click',()=> {
     moreWorks.classList.toggle("showMore");
     if (showMoreBtn.innerText === 'Show more') {
         showMoreBtn.innerText = 'Show less';
