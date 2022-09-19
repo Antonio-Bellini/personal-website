4eb78e09be948159116383776ca2a97ac43ef4ad
Ant00o <belliniantonio205@gmail.com>
2022-09-19 20:06:40 +0200

    Favicon, e visualizzazione da mobile

--- src/router/index.js
+++ src/router/index.js
@@ -1,25 +1,20 @@
-import { createRouter, createWebHashHistory } from 'vue-router'
+import { createRouter, createWebHistory } from 'vue-router'
 import Index from '../views/Index.vue'
 
-const routes = [
-  {
-    path: '/',
-    name: 'home',
-    component: Index
-  },
-  {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
-]
-
 const router = createRouter({
-  history: createWebHashHistory(),
-  routes
+  history: createWebHistory(),
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: Index
+    },
+    {
+      path: '/about',
+      name: 'about',
+      component: () => import('../views/About.vue')
+    }
+  ]
 })
 
 export default router
